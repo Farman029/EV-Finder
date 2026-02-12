d89c01e2f22f2ac1204db3adedf1b89697ad1134
Farman029 <mdfarmanansari6@gmail.com>
2026-02-12 22:33:14 +0530

    localhost: remove

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,36 +1,8 @@
-// import axios from 'axios';
-
-// const api = axios.create({
-//   baseURL: 'http://localhost:5000/api', // Connects to your Backend
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-// });
-
-// export default api;
-
-
-
-// import axios from 'axios';
-
-// // Create an axios instance
-// const API = axios.create({ baseURL: 'http://localhost:5000/api' });
-
-// // Add a request interceptor
-// API.interceptors.request.use((req) => {
-//   if (localStorage.getItem('token')) {
-//     req.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
-//   }
-//   return req;
-// });
-
-// export default API;
-
 
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: '/api',
 });
 
 // ADD THIS INTERCEPTOR
